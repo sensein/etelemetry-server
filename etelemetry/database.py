dbe3b56bc7d72ce3d61c6061a8d39bdbf16c40dd
mathiasg <mathiasg@mit.edu>
2019-08-23 09:45:48 -0400

    sty: show class initialization first

--- etelemetry/database.py
+++ etelemetry/database.py
@@ -11,21 +11,21 @@ from .utils import get_current_time
 class MongoClientHelper:
     """Helper class for writing to mongo database"""
 
+    def __init__(self):
+        self.client = amotor.AsyncIOMotorClient(
+            os.getenv("DB_HOSTNAME", 'localhost'), 27017,
+        )
+        self.db = self.client[os.getenv("ETELEMETRY_DB", "et")]
+        self.collection = self.db[os.getenv("ETELEMETRY_COLLECTION", "v1")]
+
     async def is_valid(self):
+        """Run mongo command to ensure valid connection"""
         try:
             await self.client.admin.command('ismaster')
             logger.info("Successful connection to mongo")
         except (ConnectionFailure, ServerSelectionTimeoutError):
             logger.critical("Server is not available")
             raise
-
-    def __init__(self):
-
-        self.client = amotor.AsyncIOMotorClient(
-            os.getenv("DB_HOSTNAME", 'localhost'), 27017,
-        )
-        self.db = self.client[os.getenv("ETELEMETRY_DB", "et")]
-        self.collection = self.db[os.getenv("ETELEMETRY_COLLECTION", "v1")]
 
     async def db_insert(self, host, owner, repo, version, cached, status):
         """Insert request information to db"""
