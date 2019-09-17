188073015de717b0b37698fe8ab014a49869d9d3
mathiasg <mathiasg@mit.edu>
2019-09-17 16:52:55 -0400

    sty: move import

--- etserver/__init__.py
+++ etserver/__init__.py
@@ -3,6 +3,10 @@ from pathlib import Path
 
 from sanic.log import logger
 
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
 
 CACHEDIR = Path(
     os.getenv("ETELEMETRY_CACHE") or Path.home() / '.etcache'
@@ -12,7 +16,3 @@ CACHEDIR.mkdir(parents=True, exist_ok=True)
 GITHUB_RELEASE_URL = 'https://api.github.com/repos/{owner}/{repo}/releases/latest'
 GITHUB_TAG_URL = 'https://api.github.com/repos/{owner}/{repo}/tags'
 IPSTACK_URL = 'http://api.ipstack.com/{ip}'
-
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
